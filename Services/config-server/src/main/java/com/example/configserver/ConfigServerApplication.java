d33d79763ed1305cebbab637947daa95c558a87c
mnaffeti <mohamedaziz.naffeti@esprit.tn>
2025-07-10 23:21:54 +0100

    feat: add notification & gateway service

--- Services/config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ Services/config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -3,12 +3,15 @@ package com.example.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		String token = dotenv.get("MY_SECRET_TOKEN");
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
